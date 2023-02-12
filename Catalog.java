a0a848e9c29368dec37d554d26283da949d03c96
Kirill Allyanov <kir.allyanov@gmail.com>
2023-02-12 14:49:32 +0300

    first commit

---
+++ Catalog.java
@@ -0,0 +1,11 @@
+package DZ6;
+
+import java.util.*;
+
+public class Catalog extends laptop {
+
+    public Catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
